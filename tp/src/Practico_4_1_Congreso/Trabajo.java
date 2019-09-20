b68b9d4fe39b0eabe5f3f0472fa5d8714266f852
maxisantomil <maxisantomil90@gmail.com>
2019-09-20 18:11:02 -0300

    prueba ingresando cambios

--- tp/src/Practico_4_1_Congreso/Trabajo.java
+++ tp/src/Practico_4_1_Congreso/Trabajo.java
@@ -1,7 +1,15 @@
 package Practico_4_1_Congreso;
-
+import java.util.ArrayList;
 public class Trabajo {
 
 	String articulo;
 	String resumen;
+	ArrayList<Evaluadores>Evaluador;
+	int cant;
+	boolean k;
+	
+	
 }
+
+
+
